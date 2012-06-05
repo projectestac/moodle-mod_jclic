d5cefa130734858b8ab33dbb70b6708363bb269f
Sara Arjona <sarjona@xtec.cat>
2012-06-05 09:27:00 +0200

    Upudated module version

--- version.php
+++ version.php
@@ -36,5 +36,5 @@ $module->version   = 2012060400;      // The current module version (Date: YYYYM
 $module->requires  = 2011033009;      // Requires this Moodle version (2.0)
 $module->cron      = 0;               // Period for cron to check this module (secs)
 $module->component = 'mod_jclic';     // To check on upgrade, that module sits in correct place
-$module->release   = 'v0.2.0';        // Human-readable version name
+$module->release   = 'v0.2.1';        // Human-readable version name
 $module->maturity = MATURITY_RC;      // How stable the plugin is
