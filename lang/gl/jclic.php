87e1fe89ac036e60d1d9ed0fa50351de64d7a265
Sara Arjona <sarjona@xtec.cat>
2012-08-16 10:17:11 +0200

    Added Galician translation and CHANGES.txt file

--- lang/gl/jclic.php
+++ lang/gl/jclic.php
@@ -17,7 +17,7 @@
 
 
 /**
- * Galician strings for jclic
+ * English strings for jclic
  *
  * You can have a rather longer description of the file as well,
  * if you like, and it can span multiple lines.
@@ -31,14 +31,14 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['activitydone'] = 'Actividades realizadas';
-$string['activitysolved'] = 'Activitades acertadas';
+$string['activitysolved'] = 'Actividades acertadas';
 $string['attempts'] = 'Intentos';
-$string['avaluation'] = 'Criterio de avaluaci&oacute;n';
+$string['avaluation'] = 'Criterio de avaliaci&oacute;n';
 $string['avaluation_score'] = 'Acadar unha puntuaci&oacute;n media entre todas as actividades';
 $string['avaluation_solved'] = 'Resolver correctamente un n&uacute;mero de actividades diferentes';
 $string['description'] = 'Descrici&oacute;n';
 $string['height']='Altura';
-$string['hideall']='Amosar s&oacute; os res&uacute;mos';
+$string['hideall']='Amosar s&oacute; os resumos';
 $string['lastaccess']='&uacute;ltimo acceso';
 $string['maxattempts'] = 'N&uacute;mero m&aacute;ximo de intentos';
 $string['maxgrade'] = 'Puntuaci&oacute;n/actividades a acadar';
@@ -60,61 +60,61 @@ $string['width']='Anchura';
 /* Revision 20070305 */
 $string['actions']='Acertos';
 $string['activity']='Actividade';
-$string['msg_nosessions']='Esta actividade JClic ainda non ten ningunha sesi&oacute;n';
+$string['msg_nosessions']='Esta actividade JClic a&iacute;nda non ten ningunha sesi&oacute;n';
 $string['solved']='Correcta';
 $string['time']='Tempo';
 
 /* Revision 20071002 */
 $string['header_jclic']='Axustes de JClic';
-$string['header_score']='Axustes de avaluaci&oacute;n';
+$string['header_score']='Axustes de avaliaci&oacute;n';
 
 /* Revision 20091026 */
 $string['deleteallsessions'] = 'Borrar t&oacute;dalas sesi&oacute;ns';
 
 /* Revision 20110119  - version 0.1.0.11 */
 $string['lang']='Idioma';
-$string['exiturl']='Enlace de saída';
+$string['exiturl']='Enlace de sa&iacute;da';
 
 
-/* Revision Moodle 2.X */
-$string['availabledate'] = 'Disponible desde';
-$string['closebeforeopen'] = 'No se pudo actualizar el JClic: la fecha de cierre es anterior a la de apertura.';
-$string['duedate'] = 'Fecha de entrega';
-$string['exiturl_help'] = 'Se trata del URL que se abrir&aacute; cuando el alumnado finalice la &uacute;ltima actividad JClic.
+/* Revision Moodle 2.X 20110725 */
+$string['availabledate'] = 'Dispo&ntilde;ible desde';
+$string['closebeforeopen'] = 'No se puido actualizar o JClic: a data de peche &eacute; anterior &aacute; de apertura.';
+$string['duedate'] = 'Data de entrega';
+$string['exiturl_help'] = 'Tr&aacute;tase da URL que se abrir&aacute; cando o alumnado remate a derradeira actividade JClic.
     
-Para que esta redirecci&oacute;n funcione es necesario que, en la pesta&ntilde;a "Secuencias", la &uacute;ltima actividad del projecto JClic, tenga asociada, en la secci&oacute;n "Flecha adelante", la acci&oacute;n "Salir de JClic".';
-$string['expired'] = 'Lo sentimos, esta actividad se cerró en {$a} y ya no está disponible';
+Para que esta redirecci&oacute;n funcione &eacute; necesario que, na pestana "Secuencias", a derradeira actividade do proxecto JClic, te&ntilde;a asociada, na selecci&oacute;n "Data adiante", a acci&oacute;n "Sa&iacute;r de JClic".';
+$string['expired'] = 'Sent&iacute;molo, esta actividade pechouse en {$a} e xa non est&aacute; dispo&ntilde;ible';
 $string['filetype'] = 'Tipo';
-$string['filetype_help'] = 'Este parámetro determina cómo se incluye el paquete JClic en el curso. Hay 2 opciones:
+$string['filetype_help'] = 'Este par&aacute;metro determina como se incl&uacute;e no paquete JClic no curso. Hai 2 opci&oacute;ns:
 
-* Fichero JClic subido - Posibilita escoger un fichero ".jclic.zip" válido mediante el selector de archivos.
-* URL externa - Posibilita especificar el URL de un paquete JClic. NOTA: El URL debe empezar con https(s) o www y contener un fichero ".jclic.zip" válido';
+* Ficheiro JClic subido - Posibilita escoller un ficheiro ".jclic.zip" v&aacute;lido mediante o selector de arquivos.
+* URL externa - Posibilita especificar a URL dun paquete JClic. NOTA: A URL debe comezar con https(s) ou www e conter un ficheiro ".jclic.zip" v&aacute;lido';
 $string['filetypeexternal'] = 'URL externa';
-$string['filetypelocal'] = 'Fichero JClic subido';
-$string['invalidjclicfile'] = 'Se ha especificado un fichero JClic no válido. El fichero debe tener la extensión ".jclic.zip".';
-$string['invalidurl'] = 'Se ha especificado un URL no válido. El URL debe empezar con http(s) y enlazar a un fichero ".jclic.zip" válido.';
+$string['filetypelocal'] = 'Ficheiro JClic subido';
+$string['invalidjclicfile'] = 'Especificouse un ficheiro JClic non v&aacute;lido. O ficheiro debe ter a extensi&oacute;n ".jclic.zip".';
+$string['invalidurl'] = 'Especificouse unha URL non v&aacute;lido. A URL debe comezar con http(s) e enlazar a un ficheiro ".jclic.zip" v&aacute;lido.';
 $string['jclic'] = 'JClic';
-$string['jclicjarbase']='URL base de los ficheros JAR';
-$string['jclicjarbase_help']='Dirección web donde localizar todos los ficheros jar de JClic.';
+$string['jclicjarbase']='URL base dos ficheiros JAR';
+$string['jclicjarbase_help']='Direcci&oacute;n web onde localizar t&oacute;dolos ficheiros jar de JClic.';
 $string['jclicurl'] = 'URL';
-$string['jclicurl_help'] = 'Este parámetro habilita un URL para especificar el paquete JClic en lugar de seleccionarlo a través del selector de archivos.';
-$string['jclicfile'] = 'Fichero JClic';
-$string['jclicfile_help'] = 'El fichero ".jclic.zip" que contiene el paquete JClic.';
-$string['lap']='Tiempo entre vueltas';
+$string['jclicurl_help'] = 'Este par&aacute;metro habilita unha URL para especificar o paquete JClic en lugar de seleccionalo a trav&eacute;s do selector de arquivos.';
+$string['jclicfile'] = 'Ficheiro JClic';
+$string['jclicfile_help'] = 'O ficheiro ".jclic.zip" que cont&eacute;n o paquete JClic.';
+$string['lap']='Tempo entre voltas';
 $string['lap_help']='Tempo que se deixa entre as transacci&oacute;ns cliente-servidor (expresado en segundos)';
-$string['modulename_help'] = '<a href="http://clic.xtec.cat" target="_blank">JClic</a> es un proyecto del Departament de Ensenyament de la 
-    Generalitat de Cataluña que está formado por un conjunto de aplicaciones de software libre que permiten crear diversos tipos de actividades
-    educativas multimedia: puzzles, asociaciones, ejercicios de texto, crucigramas, sopas de letras y otros. Además, la 
-    <a href="http://clic.xtec.cat/db/listact_ca.jsp" target="_blank">zonaClic</a> dispone de una biblioteca de actividades que cuenta con unos 1000 
-    proyectos que han creado profesores y personas de otros colectivos que han querido compartir solidariamente su trabajo. 
+$string['modulename_help'] = '<a href="http://clic.xtec.cat" target="_blank">JClic</a> &eacute; un proxecto do Departament de Ensenyament da 
+    Generalitat de Cataluña que est&aacute; formado por un conxunto de aplicaci&oacute;ns de software libre que permiten crear diversos tipos de actividades
+    educativas multimedia: quebracabezas, asociaci&oacute;ns, exercicios de texto, encrucillados, sopas de letras y outros. Ademais, a 
+    <a href="http://clic.xtec.cat/db/listact_ca.jsp" target="_blank">zonaClic</a> disp&oacute;n dunha biblioteca de actividades que conta cuns 1000 
+    proxectos que crearon mestres e persoas doutros colectivos que quixeron compartir solidariamente o seu traballo. 
     
-Este módulo permite al profesorado añadir a un curso cualquier actividad de tipo JClic y recopilar los resultados obtenidos (tiempo utilizado para cada actividad, intentos, aciertos, etc.) para cada alumno/a.';
-$string['notopenyet'] = 'Esta actividad no estará disponible hasta {$a}';
-$string['pluginadministration'] = 'Administración de JClic';
+Este m&oacute;dulo permite ao profesorado engadir a un curso calquera actividade de tipo JClic e recopilar os resultados obtidos (tempo empregado para cada actividade, intentos, acertos, etc.) para cada alumno/a.';
+$string['notopenyet'] = 'Esta actividade non estar&aacute; dispo&ntilde;ible ata {$a}';
+$string['pluginadministration'] = 'Administraci&oacute;n de JClic';
 $string['pluginname'] = 'JClic';
 $string['preview_jclic']='Amosar a actividade JClic'; // Previsualizar la actividad JClic
-$string['return_results']='Volver a los resultados';
+$string['return_results']='Volver aos resultados';
 $string['show_my_results']='Amosar os resultados'; // Mostrar mis resultados
 $string['solveddone'] = 'Actividades acertadas / realizadas';
-$string['urledit'] = 'Fichero JClic';
-$string['urledit_help'] = 'El fichero "jclic.zip" que contiene la actividad JClic.';
+$string['urledit'] = 'Ficheiro JClic';
+$string['urledit_help'] = 'O ficheiro "jclic.zip" que cont&eacute;n a actividade JClic.';
